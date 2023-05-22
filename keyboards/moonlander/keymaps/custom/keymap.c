a18df7ae60b96d16719a688eaf855eb37fe60e1f
ghost-of-freedom <97169326+ghost-of-freedom@users.noreply.github.com>
2023-05-22 20:35:27 +0200

    Add numbers layer

--- keyboards/moonlander/keymaps/custom/keymap.c
+++ keyboards/moonlander/keymaps/custom/keymap.c
@@ -15,16 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
-
 #include QMK_KEYBOARD_H
 #include "version.h"
 
 enum layers {
     BASE,  // default layer
     SYMB,  // symbols
-    MDIA,  // media keys
+    NUMB,  // numbers
 };
 
 enum custom_keycodes {
@@ -34,31 +31,61 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
-        KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-    LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
-    ),
+        QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
+        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_HOME,
+        
+        KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_NO,
+        KC_NO, KC_J, KC_L, KC_U, KC_Y, KC_SEMICOLON, KC_PAGE_UP,
+        
+        QK_CAPS_WORD_TOGGLE, KC_A, KC_R, KC_S, KC_T, KC_G, KC_NO,
+        KC_NO, KC_M, KC_N, KC_E, KC_I, KC_O, KC_PAGE_DOWN,
+        
+        KC_NO, KC_X, KC_C, KC_D, KC_V, KC_Z,
+        KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLASH, KC_END,
+        
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_KB_VOLUME_DOWN,
+        KC_KB_VOLUME_UP, KC_NO, KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,
+        
+        KC_SPACE, KC_ESCAPE, KC_TAB, KC_DELETE, KC_ENTER, KC_BACKSPACE
+        ),
 
     [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EE_CLR,  _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
-                                            RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
-    ),
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        
+        KC_NO, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_MINUS, KC_UNDERSCORE, KC_TILDE, KC_NO,
+        KC_NO, KC_NO, KC_EXCLAIM, KC_AT, KC_HASH, KC_NO, KC_NO,
+        
+        KC_NO, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_QUOTE, KC_DOUBLE_QUOTE, KC_TILDE, KC_NO,
+        KC_NO, KC_NO, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_PIPE, KC_NO,
+        
+        KC_NO, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_GRAVE, KC_EQUAL, KC_NO,
+        KC_NO, KC_AMPERSAND, KC_BACKSLASH, KC_SLASH, KC_NO, KC_NO,
+        
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        
+        _______, _______, _______, _______, _______, _______
+        ),
 
-    [MDIA] = LAYOUT_moonlander(
-        LED_LEVEL,_______,_______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, KC_MS_U, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
-        _______, _______, _______, _______, _______, _______,                             _______, _______, KC_MPRV, KC_MNXT, _______, _______,
-        _______, _______, _______, KC_BTN1, KC_BTN2,         _______,            _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
-                                            _______, _______, _______,           _______, _______, _______
-    ),
+    [NUMB] = LAYOUT_moonlander(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO,
+
+        KC_NO, KC_SLASH, KC_ASTERISK, KC_EQUAL, KC_PLUS, KC_MINUS, KC_NO,
+        KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_DOT, KC_NO,
+
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO,
+
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_0, KC_0, KC_0, KC_NO, KC_NO,
+        
+        _______, _______, _______, _______, _______, _______
+        ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
